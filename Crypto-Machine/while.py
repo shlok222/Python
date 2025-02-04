8f7fe094db90c71cab1e057f948a1a06d5e78686
Shlok Bhargude <shlokbhargude7@gmail.com>
2025-02-04 20:11:32 +0530

    More Code Added

--- Crypto-Machine/while.py
+++ Crypto-Machine/while.py
@@ -63,3 +63,46 @@ elif a == 6:
     print('The opposite side of the dice for',a,'is 1')
 else:
     print('Enter Wrong Input')
+
+# Sum of GP (Geometric Series)
+
+# A Geometric series is a series with a constant ratio between successive terms.
+# The first term of the series is denoted by a and common ratio is denoted by r. 
+#The series looks like this :- a, ar, ar2, ar3, ar4, . . .. 
+#The task is to find the sum of such a series. Examples :
+
+# Input : a = 1
+#         r = 0.5
+#         n = 10
+# Output : 0.9990234375
+
+a = 1
+r = 0.5
+n = 10
+Sum = 0
+
+for i in range(1,n+1):
+    Sum += a*(r**i)
+print(Sum)
+
+# Simple Intrest
+
+# Given Principal p, Rate r and Time t, the task is to calculate Simple Interest.
+
+# Input: p = 10000, r = 5, t = 5 
+# Output:2500 
+# Explanation: We need to find simple interest on  Rs. 10,000 at the rate of 5% for 5  units of time. 
+
+p = 10000
+r = 5
+t = 5
+
+SI = (p * t * r)/100
+print(SI)
+
+# Area of Circle
+
+Radius = eval(input('Enter the radius of circle'))
+
+Area = 3.14 * Radius ** 2
+print('Area of circle with raduis',Radius,'is',Area)
