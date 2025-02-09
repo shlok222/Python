f68520cdcbcb8466157e8c76204690906b4f6ccc
Shlok Bhargude <shlokbhargude7@gmail.com>
2025-02-09 23:02:36 +0530

    For loop codes

--- Crypto-Machine/tp.py
+++ Crypto-Machine/tp.py
@@ -4,3 +4,16 @@ a = eval(input('Enter a no'))
 
 for i in range(1,11):
     print(a,'*',i,'=',a * i)
+
+# Sum of Natural Nos
+
+# Input: n = 3
+# Output: 6
+# Explanation: Note that 1 + 2 + 3 = 6
+
+a = eval(input('Enter a no'))
+Sum = 0
+
+for i in range(1,a + 1):
+    Sum += i
+print(Sum)
